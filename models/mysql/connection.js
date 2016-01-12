20ed5173e069468a54e4bb8bd5fbc0cce4c9f7f4
tanshaohui <tanshaohui@baidu.com>
2016-01-12 13:18:01 +0800

    commit

--- models/mysql/connection.js
+++ models/mysql/connection.js
@@ -3,7 +3,7 @@
  * @author tanshaohui <tanshaohui@baidu.com>
  * @date 2016-01-12 10:57:37
  * @last-modified-by tanshaohui
- * @last-modified-time 2016-01-12 12:17:31
+ * @last-modified-time 2016-01-12 13:17:39
  */
 
 var mysql = require('mysql');
@@ -11,7 +11,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'tsh0721.'
+    password: 'yunroot'
 });
 
 var CREATE_DATABASE = 'CREATE DATABASE IF NOT EXISTS my_app';
